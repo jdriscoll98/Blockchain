7999052f8b3be3c36faf145943f859a9c019f8c6
Jack Driscoll <jdriscoll98@ufl.edu>
2021-10-16 16:11:00 -0400

    Basic Blockchain with proof of work

---
+++ main.js
@@ -0,0 +1,77 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== "0".repeat(difficulty)) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+      console.log("current hash", this.hash);
+    }
+
+    console.log("Block mined: " + this.hash);
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, new Date(), "Genesis Block", null);
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+const titancoin = new Blockchain();
+console.log("Mining block 1...");
+titancoin.addBlock(new Block(1, new Date(), { amount: 1 }));
+
+console.log("Mining block 2...");
+titancoin.addBlock(new Block(2, new Date(), { amount: 2 }));
